0d6b191d15d0e1ceb5e7633146aee11bf9cca865
Sneha Verma <sverma1012@gmail.com>
2025-01-08 22:21:25 -0600

    remove unnecessary files.

--- readmore.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".showmore").forEach(function (p) {
-  p.querySelector("a").addEventListener("click", function () {
-    p.classList.toggle("show");
-    this.textContent = p.classList.contains("show") ? "Show Less" : "Show More";
-  });
-});
